3c4de2cd4457d1199913bcbe722865b0e7e0ce67
Elvin Ega <proega3@gmail.com>
2024-03-29 15:36:40 +0300

    add dark theme

--- src/components/dashboard/NavBar.tsx
+++ src/components/dashboard/NavBar.tsx
@@ -27,6 +27,7 @@ import {
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Link, NavLink } from "react-router-dom";
+import { ModeToggle } from "../ModeToggle";
 const Navbar = () => {
   return (
     <>
@@ -43,9 +44,9 @@ const Navbar = () => {
                 />
                 {/* <span className="">Kazifi</span> */}
               </Link>
+
               <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                <Bell className="h-4 w-4" />
-                <span className="sr-only">Toggle notifications</span>
+                <ModeToggle />
               </Button>
             </div>
             <div className="flex-1">
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./components/ui/theme-provider";
 import "non.geist";
 import "non.geist/mono";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
